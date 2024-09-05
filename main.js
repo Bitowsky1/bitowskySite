7b9728ffd0ff2cc4888db53ae784d40181c793ea
Bitowsky1 <135824295+Bitowsky1@users.noreply.github.com>
2024-09-05 15:01:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function goToLinktree () {
     window.location.replace("https://linktr.ee/bitowsky");
 }
+
+console.log("%cNo, you won't find anything here as well ;)", "background: #000000; color: #00ff00");
