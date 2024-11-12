406a887716d2f06c1e2d868546240a2c3c5ea3cf
Bitowsky2 <67092848+Bitowsky@users.noreply.github.com>
2024-11-12 20:41:36 +0100

    added sheet of commissions

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ function goToLinktree () {
     window.location.replace("https://linktr.ee/bitowsky");
 }
 
+function goToSheet () {
+    window.location.replace("https://docs.google.com/spreadsheets/d/1HWvzUHsKtbPbg2ii4mshJOYiWLwO8Ppgs2K8DHMT7UM/edit?usp=sharing");
+}
+
 console.log("%cNo, you won't find anything here as well ;)", "background: #000000; color: #00ff00");
