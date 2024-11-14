96b39ac5cf339435c850a2697598a86619a10e58
Bitowsky2 <67092848+Bitowsky@users.noreply.github.com>
2024-11-14 21:24:06 +0100

    added link to drive

--- main.js
+++ main.js
@@ -6,4 +6,8 @@ function goToSheet () {
     window.location.replace("https://docs.google.com/spreadsheets/d/1HWvzUHsKtbPbg2ii4mshJOYiWLwO8Ppgs2K8DHMT7UM/edit?usp=sharing");
 }
 
+function goToDrive () {
+    window.location.replace("http://drive.google.com/drive/folders/13YPsj65yKY4pYFmfjqz3ew4zgpUaRuIr?usp=sharing");
+}
+
 console.log("%cNo, you won't find anything here as well ;)", "background: #000000; color: #00ff00");
