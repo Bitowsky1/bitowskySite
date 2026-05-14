7c2fd62c45a9a36b967e72ceac0e43729dcbe456
Bitowsky2 <67092848+Bitowsky@users.noreply.github.com>
2026-05-15 00:07:12 +0200

    added crushedInDreams

---
+++ subsites/crushedInDreams/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,14 @@
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event1_Act2(runtime, localVars)
+	{
+		console.log("%cHolmes, I think I'm having a heart attack! Is this world also fake!? Am I also fake!?", "color: lightblue; font-size: x-large");
+		console.log("%cWatson! Stop being paranoid! I bet it's just some random fan of our adventures trying to use the totally reliable and definitely not flawed 3D system of Construct 3.", "color: gold; font-size: x-large");
+		console.log("%cWhat makes you think that?", "color: lightblue; font-size: x-large");
+		console.log("%cThe \"Console.log()\" function, Watson. The \"Console.log()\" function.", "color: gold; font-size: x-large");
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
